0e838d4c43f22ba44ec64a82365d6746eab10fb0
theodoremoreland <theodoremoreland@yahoo.com>
2024-01-14 15:33:21 -0600

    removed github and linkedin links as well as app summary

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { getRepoData } from "./App.controller.js";
 
 // Third party
 import { useQuery } from "@tanstack/react-query";
-import Image from "react-bootstrap/Image";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
@@ -17,10 +16,6 @@ import TimelineLoadingScreen from "./components/Timeline/LoadingScreen/LoadingSc
 // Custom
 import extractErrorMessage from "./utils/extractErrorMessage.js";
 import backupData from "./data/backup-data.json";
-
-// Images
-import octocatIcon from "./images/Octocat.png";
-import linkedInIcon from "./images/LI-In-Bug.png";
 
 // Custom Styles
 import "./reset.css";
@@ -89,42 +84,6 @@ function App() {
         )}
       </header>
       <main className="content">
-        {/* <header className="timelineContainerHeader">
-          <h2 className="timelineContainerTitle">Projects</h2>
-          <ul>
-            <li className="timelineContainerSubtitle">
-              A dynamically generated list of my GitHub projects via GitHub API.
-            </li>
-            <li className="timelineContainerSubtitle">
-              Projects can be filtered and sorted.
-            </li>
-            <li className="timelineContainerSubtitle">
-              Click on the blue icon next to a project's name to render its
-              README.
-            </li>
-            <li className="timelineContainerSubtitle">
-              Click on the project's thumbnail to view its code on GitHub.
-            </li>
-          </ul>
-          <div className="headerIconsContainer">
-            <a
-              href="https://github.com/theodoremoreland"
-              rel="noopener noreferrer"
-              target="_blank"
-              title="My GitHub Profile"
-            >
-              <Image className="octocatIcon" src={octocatIcon} fluid />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/theodore-moreland/"
-              rel="noopener noreferrer"
-              target="_blank"
-              title="My LinkedIn Profile"
-            >
-              <Image className="linkedInIcon" src={linkedInIcon} fluid />
-            </a>
-          </div>
-        </header> */}
         {repos !== undefined ? (
           <Timeline projects={repos} />
         ) : (
