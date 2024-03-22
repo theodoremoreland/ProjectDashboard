76515bea32e4e0b8d7735236c1a211f9569b7e52
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-22 18:11:04 -0500

    added ProjectsContext

---
+++ src/components/TechList/TechList.controller.js
@@ -0,0 +1,37 @@
+const frameworks = {
+  flask: "Flask",
+  react: "React",
+  "react-native": "React-Native",
+  "spring-boot": "Spring-Boot",
+  junit: "jUnit",
+  jest: "Jest",
+  jasmine: "Jasmine",
+  unittest: "unittest",
+};
+const competencies = {
+  "web-development": "web-development",
+  "mobile-development": "mobile-development",
+  "data-engineering": "data-engineering",
+  "data-analytics": "data-analytics",
+};
+const languages = {
+  python: "Python",
+  javascript: "JavaScript",
+  java: "Java",
+  typescript: "TypeScript",
+  sql: "SQL",
+  plpgsql: "PLpgSQL",
+  vba: "VBA",
+  bash: "Bash",
+  html: "HTML",
+  css: "CSS",
+};
+const tools = {
+  node: "Node",
+  pandas: "Pandas",
+  docker: "Docker",
+  postgresql: "PostgreSQL",
+  mysql: "MySQL",
+  mongodb: "MongoDB",
+  matplotlib: "Matplotlib",
+};
---
+++ src/http/getRepoData.js
@@ -0,0 +1,44 @@
+// GitHub
+import { Octokit } from "@octokit/core";
+
+// Scripts
+import covertDateToISOString from "../utils/convertDateToISOString";
+
+const accessToken = process.env.REACT_APP_GITHUB_API_ACCESS_TOKEN;
+const octokit = new Octokit({ auth: accessToken });
+
+export const getRepoData = async () => {
+  const response = await octokit.request(
+    "GET /user/repos?per_page=100&affiliation=owner&visibility=public&sort=created",
+    {
+      owner: "octocat",
+      repo: "hello-world",
+      mediaType: {
+        previews: ["mercy"],
+      },
+    }
+  );
+
+  const repoDataArray = response.data.reduce((repos, repo) => {
+    const repoData = {
+      name: repo.name,
+      url: repo.html_url,
+      desc: repo.description,
+      date_created: covertDateToISOString(repo.created_at),
+      date_updated: covertDateToISOString(repo.pushed_at),
+      topics: repo.topics,
+      image: `https://raw.githubusercontent.com/theodoremoreland/${repo.name}/master/presentation/thumbnail.png`,
+      demo_link: repo.homepage,
+      readme: `https://raw.githubusercontent.com/theodoremoreland/${repo.name}/master/README.md`,
+      size: Number(repo.size),
+    };
+
+    if (repoData.topics.length > 0) {
+      return [...repos, repoData];
+    } else {
+      return repos;
+    }
+  }, []);
+
+  return repoDataArray;
+};
