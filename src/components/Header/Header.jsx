29f38ba7191531aaa41ce0c6a9d2a3b182019052
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-22 04:39:05 -0500

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Styles
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header id="header">
+
+        </header>
+    )
+};
+
+
+export default Header;
