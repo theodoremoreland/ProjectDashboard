312ff096317ba9e9e1586fd80cc900d7c208f69c
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-22 18:49:36 -0500

    added data to tech list

--- src/components/TechList/TechList.jsx
+++ src/components/TechList/TechList.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext, useMemo } from 'react';
+
+// Controller
+import { getTopicCounts } from './TechList.controller';
+
+// Context
+import { ProjectsContext } from '../../contexts/ProjectsContext'; 
 
 // Styles
 import './TechList.css';
 
 const TechList = () => {
+    const { repos } = useContext(ProjectsContext);
+    const topicsCount = useMemo(() => getTopicCounts(repos), [repos]);
+
     return (
-        <ul id="tech-list"></ul>
+        <ul id="tech-list">
+            <p>Languages</p>
+            {Object.entries(topicsCount.languages).map(([k, v]) => <li>{k}:{v}</li>)}
+            <p>Frameworks</p>
+            {Object.entries(topicsCount.frameworks).map(([k, v]) => <li>{k}:{v}</li>)}
+            <p>Competencies</p>
+            {Object.entries(topicsCount.competencies).map(([k, v]) => <li>{k}:{v}</li>)}
+            <p>Tools</p>
+            {Object.entries(topicsCount.tools).map(([k, v]) => <li>{k}:{v}</li>)}
+        </ul>
     )
 };
 
