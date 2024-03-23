c9e5182e721191c09062ac622278da0b0aaa1fa7
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-23 04:29:29 -0500

    fixed bug with techlist initial render

--- src/components/TechList/TechList.jsx
+++ src/components/TechList/TechList.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 // Controller
 import { getTopicCounts } from './TechList.controller';
@@ -11,18 +11,24 @@ import './TechList.css';
 
 const TechList = () => {
     const { repos } = useContext(ProjectsContext);
-    const topicsCount = useMemo(() => getTopicCounts(repos), [repos]);
+    const [topicsCount, setTopicsCount] = useState(null);
+
+    useEffect(() => {
+        if (repos) {
+            setTopicsCount(getTopicCounts(repos));
+        };
+    }, [repos]);
 
     return (
         <ul id="tech-list">
             <p>Languages</p>
-            {Object.entries(topicsCount.languages).map(([k, v]) => <li>{k}:{v}</li>)}
+            {topicsCount && Object.entries(topicsCount.languages).map(([k, v]) => <li>{k}: {v}</li>)}
             <p>Frameworks</p>
-            {Object.entries(topicsCount.frameworks).map(([k, v]) => <li>{k}:{v}</li>)}
+            {topicsCount && Object.entries(topicsCount.frameworks).map(([k, v]) => <li>{k}: {v}</li>)}
             <p>Competencies</p>
-            {Object.entries(topicsCount.competencies).map(([k, v]) => <li>{k}:{v}</li>)}
+            {topicsCount && Object.entries(topicsCount.competencies).map(([k, v]) => <li>{k}: {v}</li>)}
             <p>Tools</p>
-            {Object.entries(topicsCount.tools).map(([k, v]) => <li>{k}:{v}</li>)}
+            {topicsCount && Object.entries(topicsCount.tools).map(([k, v]) => <li>{k}: {v}</li>)}
         </ul>
     )
 };
