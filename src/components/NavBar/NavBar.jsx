96f283c6221abefab66789a2fed50dbfd515bbf7
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-24 01:52:55 -0500

    added links to nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+// Bootstrap
+import Image from "react-bootstrap/Image";
+
+// Images
+import octocatIcon from "../../images/Octocat.png";
+import linkedInIcon from "../../images/LI-In-Bug.png";
+
 // Styles
 import './NavBar.css';
 
@@ -7,6 +14,28 @@ const NavBar = () => {
     return (
         <nav id="app-nav">
             <h1>Project List</h1>
+            <ul>
+                <li>
+                <a
+            href="https://github.com/theodoremoreland"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="My GitHub Profile"
+          >
+            <Image className="octocatIcon" src={octocatIcon} fluid />
+          </a>
+                </li>
+                <li>
+                <a
+            href="https://www.linkedin.com/in/theodore-moreland/"
+            rel="noopener noreferrer"
+            target="_blank"
+            title="My LinkedIn Profile"
+          >
+            <Image className="linkedInIcon" src={linkedInIcon} fluid />
+          </a>
+                </li>
+            </ul>
         </nav>
     )
 };
