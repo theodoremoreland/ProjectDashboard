bd8590621b65810bafbe49776bec56f6527f1361
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-24 01:44:02 -0500

    tech list style update

--- src/components/TechList/TechList.jsx
+++ src/components/TechList/TechList.jsx
@@ -21,15 +21,15 @@ const TechList = () => {
 
     return (
         <ul id="tech-list">
-            <p id="live-demos">Live Demos: <span className='count'>{getLiveDemosCount(repos)}</span><span id="circle"></span></p>
+            <p id="live-demos">Live Demos:<span className='count'>{getLiveDemosCount(repos)}</span></p>
             <p className='tech-category'>Competencies</p>
-            {topicsCount && Object.entries(topicsCount.competencies).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}: <span className='count'>{v}</span></li>)}
+            {topicsCount && Object.entries(topicsCount.competencies).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
             <p className='tech-category'>Languages</p>
-            {topicsCount && Object.entries(topicsCount.languages).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}: <span className='count'>{v}</span></li>)}
+            {topicsCount && Object.entries(topicsCount.languages).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
             <p className='tech-category'>Frameworks</p>
-            {topicsCount && Object.entries(topicsCount.frameworks).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}: <span className='count'>{v}</span></li>)}
+            {topicsCount && Object.entries(topicsCount.frameworks).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
             <p className='tech-category'>Tools</p>
-            {topicsCount && Object.entries(topicsCount.tools).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}: <span className='count'>{v}</span></li>)}
+            {topicsCount && Object.entries(topicsCount.tools).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
         </ul>
     )
 };
