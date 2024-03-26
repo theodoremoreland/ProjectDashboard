66b57f8a02d25d85ac2cc2bc49b46f71789d9bb1
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-26 18:18:34 -0500

    project overlay now slides in on hover, still need to polish

--- src/components/ProjectGrid/Project/Project.jsx
+++ src/components/ProjectGrid/Project/Project.jsx
@@ -26,25 +26,27 @@ const Project = ({ projectData }) => {
         }}
       />
       <div className="project-overlay"> 
-        <div className="row">
-          <ul>
-            {projectData.demo_link && 
-              <li>
-                <a href={projectData.demo_link} target="_blank" rel="noreferrer">
-                  Live Demo
-                </a>
+        <div id="content">
+          <div className="row">
+            <ul>
+              {projectData.demo_link && 
+                <li>
+                  <a href={projectData.demo_link} target="_blank" rel="noreferrer">
+                    Live Demo
+                  </a>
+                </li>
+              }
+              <li onClick={() => {}}>
+                Learn More
               </li>
-            }
-            <li onClick={() => {}}>
-              Learn More
-            </li>
-          </ul>
-        </div>
-        <div className="row">
-          <h2 className="project-name">{projectData.name}</h2>
-        </div>
-        <div className="row">
-          <ul className="language-icons"></ul>
+            </ul>
+          </div>
+          <div className="row">
+            <h2 className="project-name">{projectData.name}</h2>
+          </div>
+          <div className="row">
+            <ul className="language-icons"></ul>
+          </div>
         </div>
       </div>
     </Card>
