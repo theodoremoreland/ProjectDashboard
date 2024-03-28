f7265cdebfa55862d59effc325092ab8beada0f5
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-28 09:42:25 -0500

    added keys and titles to language list items for project overlay

--- src/components/ProjectGrid/Project/Project.jsx
+++ src/components/ProjectGrid/Project/Project.jsx
@@ -18,21 +18,23 @@ import sqlIcon from "../../../images/languages/sql.png";
 import "./Project.css";
 
 const renderLanguageIcon = (topic) => {
+  const titleTemplate = "This project uses ";
+
   switch (topic) {
     case "html":
-      return <li><img src={htmlIcon} alt="HTML" className="language-icon" /></li>;
+      return <li key={topic} title={titleTemplate + "HTML"}><img src={htmlIcon} alt="HTML" className="language-icon" /></li>;
     case "javascript":
-      return <li><img src={javascriptIcon} alt="JavaScript" className="language-icon" /></li>;
+      return <li key={topic} title={titleTemplate + "JavaScript"}><img src={javascriptIcon} alt="JavaScript" className="language-icon" /></li>;
     case "typescript":
-      return <li><img src={typescriptIcon} alt="TypeScript" className="language-icon" /></li>;
+      return <li key={topic} title={titleTemplate + "TypeScript"}><img src={typescriptIcon} alt="TypeScript" className="language-icon" /></li>;
     case "python":
-      return <li><img src={pythonIcon} alt="Python" className="language-icon" /></li>;
+      return <li key={topic} title={titleTemplate + "Python"}><img src={pythonIcon} alt="Python" className="language-icon" /></li>;
     case "java":
-      return <li><img src={javaIcon} alt="Java" className="language-icon" /></li>;
+      return <li key={topic} title={titleTemplate + "Java"}><img src={javaIcon} alt="Java" className="language-icon" /></li>;
     case "vba":
-      return <li key={topic}><img src={vbaIcon} alt="VBA" className="language-icon" /></li>;
+      return <li key={topic} title={titleTemplate + "VBA"}><img src={vbaIcon} alt="VBA" className="language-icon" /></li>;
     case "sql":
-      return <li key={topic}><img src={sqlIcon} alt="SQL" className="language-icon" /></li>;
+      return <li key={topic} title={titleTemplate + "SQL"}><img src={sqlIcon} alt="SQL" className="language-icon" /></li>;
     default:
       return null;
   }
