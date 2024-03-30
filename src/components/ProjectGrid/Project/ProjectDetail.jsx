08bc6d8edffb302c3276efde97108a0b21fdc956
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-29 23:57:42 -0500

    redesigning project detail component

--- src/components/ProjectGrid/Project/ProjectDetail.jsx
+++ src/components/ProjectGrid/Project/ProjectDetail.jsx
@@ -8,16 +8,23 @@ import "./ProjectDetail.css";
 
 const ProjectDetail = ({ projectData }) => {
     return (
-        <section className="project-detail">
-            <h1 className="project-detail__title">{projectData.name}</h1>
-            <p className="project-detail__description">{projectData.description}</p>
-
-            <h2>README.md preview</h2>
-            <p>Below is a preview of this project's root README. Some functionality may be lost.</p>
-            <article>
-                <README name={projectData.name} link={projectData.readme} />
-            </article>
-        </section>
+        <div className="project-detail">
+            <header><h1>Project List</h1><span>X</span></header>
+            <section className="project-detail__header">
+                <div>
+                    <img className="project-detail__image" src={projectData.image} alt={projectData.name} />
+                </div>
+                <div>
+                    <h1 className="project-detail__title">{projectData.name}</h1>
+                    <p className="project-detail__description">{projectData.description}</p>
+                </div>
+            </section>
+            <footer className="project-detail__footer">
+                <ul>
+                    { projectData.topics.map((topic) => <li key={topic}>{topic}</li>) }
+                </ul>
+            </footer>
+        </div>
     );
 };
 
