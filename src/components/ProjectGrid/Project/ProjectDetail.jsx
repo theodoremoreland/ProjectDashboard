826aab99cbf84ee1baf8edb86470b41f333db7d8
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-30 01:21:55 -0500

    progress of project detail style

--- src/components/ProjectGrid/Project/ProjectDetail.jsx
+++ src/components/ProjectGrid/Project/ProjectDetail.jsx
@@ -9,16 +9,26 @@ import "./ProjectDetail.css";
 const ProjectDetail = ({ projectData }) => {
     return (
         <div className="project-detail">
-            <header><h1>Project List</h1><span>X</span></header>
-            <section className="project-detail__header">
+            <header>
+                <span>X</span>
+            </header>
+            <article>
                 <div>
                     <img className="project-detail__image" src={projectData.image} alt={projectData.name} />
                 </div>
                 <div>
                     <h1 className="project-detail__title">{projectData.name}</h1>
-                    <p className="project-detail__description">{projectData.description}</p>
+                    <p className="project-detail__description">{projectData.desc}</p>
+                    <ul>
+                        <li>Date created: {projectData.date_created}</li>
+                        <li>Date updated: {projectData.date_updated}</li>
+                    </ul>
+                    <ul>
+                        <li>Source code</li>
+                        <li>README</li>
+                    </ul>
                 </div>
-            </section>
+            </article>
             <footer className="project-detail__footer">
                 <ul>
                     { projectData.topics.map((topic) => <li key={topic}>{topic}</li>) }
