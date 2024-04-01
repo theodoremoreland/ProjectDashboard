a06ab06d1fabfb7862a6774f64a0cd44218d39d5
theodoremoreland <theodoremoreland@yahoo.com>
2024-03-31 22:43:41 -0500

    removed labels for links on project details

--- src/components/ProjectGrid/Project/ProjectDetail.jsx
+++ src/components/ProjectGrid/Project/ProjectDetail.jsx
@@ -11,39 +11,54 @@ import { ReactComponent as PreviewIcon } from "../../../images/preview.svg";
 import "./ProjectDetail.css";
 
 const ProjectDetail = ({ projectData, handleClose }) => {
+    const [shouldShowReadme, setShouldShowReadme] = React.useState(false);
+
     return (
         <div className="project-detail">
             <header>
                 <button type="button" title="Close" className="x" onClick={handleClose}>x</button>
             </header>
-            <article>
-                <div>
-                    <img className="project-detail__image" src={projectData.image} alt={projectData.name} />
-                </div>
-                <div>
-                    <h1 className="project-detail__title">{projectData.name}</h1>
-                    <p className="project-detail__description">{projectData.desc}</p>
-                    <table className="metadata">
-                        <tbody>
-                            <tr>
-                                <td>Created</td>
-                                <td>{projectData.date_created}</td>
-                            </tr>
-                            <tr>
-                                <td>Last modified</td>
-                                <td>{projectData.date_updated}</td>
-                            </tr>
-                            <tr>
-                                <td>Stars</td>
-                                <td>{projectData.stars}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <ul className="topics" title="GitHub topic associated with this project.">
-                        { projectData.topics.map((topic) => <li key={topic} className="topic">{topic}</li>) }
-                    </ul>
-                </div>
-            </article>
+            {
+                shouldShowReadme && 
+                <article>
+                    <README 
+                        name={projectData.name}
+                        link={projectData.readme}
+                        handleClose={() => setShouldShowReadme(false)}
+                    />
+                </article>
+            }
+            {
+                !shouldShowReadme &&
+                <article>
+                    <div className="project-detail__image-container">
+                        <img className="project-detail__image" src={projectData.image} alt={projectData.name} />
+                    </div>
+                    <div className="project-detail__info-container">
+                        <h1 className="project-detail__title">{projectData.name}</h1>
+                        <p className="project-detail__description">{projectData.desc}</p>
+                        <table className="metadata">
+                            <tbody>
+                                <tr>
+                                    <td>Created</td>
+                                    <td>{projectData.date_created}</td>
+                                </tr>
+                                <tr>
+                                    <td>Last modified</td>
+                                    <td>{projectData.date_updated}</td>
+                                </tr>
+                                <tr>
+                                    <td>Stars</td>
+                                    <td>{projectData.stars}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <ul className="topics" title="GitHub topic associated with this project.">
+                            { projectData.topics.map((topic) => <li key={topic} className="topic">{topic}</li>) }
+                        </ul>
+                    </div>
+                </article>
+            }
             <footer className="project-detail__footer">
                 <ul className="links">
                     {projectData.demo_link && 
@@ -56,19 +71,21 @@ const ProjectDetail = ({ projectData, handleClose }) => {
                         >
                             <li>
                                 <DeployedCodeIcon className="deployment-icon" />
-                                Deployment
                             </li>
                         </a>
                     }
-                    <a href={projectData.url} target="_blank" rel="noreferrer">
+                    <a 
+                        href={projectData.url}
+                        target="_blank"
+                        rel="noreferrer"
+                        title={`Click to view the source code for ${projectData.name}.`}
+                    >
                         <li>
                             <CodeIcon className="code-icon" />
-                            Source code
                         </li>
                     </a>
-                    <li>
+                    <li role="presentation" onClick={() => setShouldShowReadme(!shouldShowReadme)}>
                         <PreviewIcon />
-                        <span>Preview README</span>
                     </li>
                 </ul>
             </footer>
