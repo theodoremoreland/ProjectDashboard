26b76035218141255e787d134776090c622db815
theodoremoreland <theodoremoreland@yahoo.com>
2024-04-02 00:12:10 -0500

    refactor: techlist component now more DRY

--- src/components/TechList/TechList.jsx
+++ src/components/TechList/TechList.jsx
@@ -9,6 +9,13 @@ import { ProjectsContext } from '../../contexts/ProjectsContext';
 // Styles
 import './TechList.css';
 
+const generateListItems = (topics) => {
+    return Object
+        .entries(topics)
+        .sort(([k1, v1], [k2, v2]) => v2 - v1)
+        .map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>);
+};
+
 const TechList = () => {
     const { repos } = useContext(ProjectsContext);
     const [topicsCount, setTopicsCount] = useState(null);
@@ -23,13 +30,13 @@ const TechList = () => {
         <ul id="tech-list">
             <p id="live-demos">Live Demos:<span className='count'>{getLiveDemosCount(repos)}</span></p>
             <p className='tech-category'>Competencies</p>
-            {topicsCount && Object.entries(topicsCount.competencies).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
+            {topicsCount && generateListItems(topicsCount.competencies)}
             <p className='tech-category'>Languages</p>
-            {topicsCount && Object.entries(topicsCount.languages).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
+            {topicsCount && generateListItems(topicsCount.languages)}
             <p className='tech-category'>Frameworks</p>
-            {topicsCount && Object.entries(topicsCount.frameworks).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
+            {topicsCount && generateListItems(topicsCount.frameworks)}
             <p className='tech-category'>Tools</p>
-            {topicsCount && Object.entries(topicsCount.tools).sort(([k1, v1], [k2, v2]) => v2 - v1).map(([k, v]) => <li key={k}>{k}:<span className='count'>{v}</span></li>)}
+            {topicsCount && generateListItems(topicsCount.tools)}
         </ul>
     )
 };
