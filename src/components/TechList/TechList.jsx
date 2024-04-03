b133771e33a49a038f779155abb2cfe297810627
theodoremoreland <theodoremoreland@yahoo.com>
2024-04-03 08:52:42 -0500

    refactor of TechList

--- src/components/TechList/TechList.jsx
+++ src/components/TechList/TechList.jsx
@@ -10,21 +10,23 @@ import { ProjectsContext } from '../../contexts/ProjectsContext';
 import './TechList.css';
 
 const TechList = () => {
-    const { repos, updateFeaturedTopics } = useContext(ProjectsContext);
+    const { repos, updateFeaturedTopics, featuredTopics } = useContext(ProjectsContext);
     const [topicsCount, setTopicsCount] = useState(null);
 
     const generateListItems = useCallback((topics) => {
         return Object
             .entries(topics)
-            .sort(([k1, v1], [k2, v2]) => v2 - v1)
-            .map(([k, v]) => <li key={k} data-filter-active="false" onClick={(e) => {
-                const target = e.currentTarget;
-
-                target.dataset.filterActive = target.dataset.filterActive === 'true' ? 'false' : 'true';
-
-                updateFeaturedTopics(findKeyForTopicLabel(k));
-              }}>{k}:<span className='count'>{v}</span></li>);
-    }, [updateFeaturedTopics]);
+            .sort(([topicLabel1, topicCount1], [topicLabel2, topicCount2]) => topicCount2 - topicCount1)
+            .map(([topicLabel, topicCount]) => 
+                <li
+                    key={topicLabel}
+                    className={`${featuredTopics?.has(findKeyForTopicLabel(topicLabel)) ? 'selected' : ''}`}
+                    onClick={() => updateFeaturedTopics(findKeyForTopicLabel(topicLabel))}
+                >
+                    {topicLabel}:<span className='count'>{topicCount}</span>
+                </li>
+                );
+    }, [updateFeaturedTopics, featuredTopics]);
 
     useEffect(() => {
         if (repos) {
