d0130422f7cb419d0f2622125a10e4e3a839dd1f
theodoremoreland <theodoremoreland@yahoo.com>
2024-04-05 22:01:58 -0500

    implemented fuse fuzzy search in SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,8 @@
-import React, { useState, useContext, useCallback } from 'react';
+import React, { useState, useContext, useCallback, useEffect } from 'react';
+
+// Third party
+import { debounce } from 'lodash';
+import Fuse from 'fuse.js';
 
 // Context
 import { ProjectsContext } from '../../contexts/ProjectsContext';
@@ -10,11 +14,24 @@ import './SearchBar.css';
 const SearchBar = () => {
     const { repos, setSelectedProject } = useContext(ProjectsContext);
     const [searchValue, setSearchValue] = useState('');
+    const [searchResults, setSearchResults] = useState(repos);
 
     const handleSearchResultClick = useCallback((projectData) => {
       setSearchValue('');
       setSelectedProject(projectData);
     }, [setSelectedProject]);
+
+    useEffect(() => {
+      if (searchValue && repos) {
+        const fuse = new Fuse(repos, {
+          keys: ['name', 'topics'],
+          threshold: 0.3
+        });
+        const results = fuse.search(searchValue);
+        setSearchResults(results);
+      }
+
+    }, [searchValue, repos]);
 
     return (
         <div id="search-bar">
@@ -29,18 +46,20 @@ const SearchBar = () => {
             searchValue &&
             <ul id='search-results'>
             {
-              repos ? repos
-                .map((repo) => 
-                <li
-                  key={repo.name}
-                  className='search-result'
-                  onClick={() => handleSearchResultClick(repo)}
-                >
-                  <p className='title'>{repo.name}</p>
-                  <div className='topics'>
-                    {repo.topics.map((topic) => <span key={topic} className='topic'>{topic}</span>)}
-                  </div>
-                </li>)
+              searchResults ? searchResults
+                .map((searchResult) => {
+                  const repo = searchResult.item;
+                  return <li
+                          key={repo.name}
+                          className='search-result'
+                          onClick={() => handleSearchResultClick(repo)}
+                        >
+                          <p className='title'>{repo.name}</p>
+                          <div className='topics'>
+                            {repo.topics.map((topic) => <span key={topic} className='topic'>{topic}</span>)}
+                          </div>
+                        </li>
+                })
                 : <li className='search-result'>No results found</li>
             }
           </ul>
