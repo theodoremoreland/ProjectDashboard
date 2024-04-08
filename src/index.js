88a05e683b8f22213e12dc189860a5691ffdb1e0
theodoremoreland <theodoremoreland@yahoo.com>
2024-04-07 20:44:42 -0500

    updated root rendering pattern to conform to React 18 standard

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
+// React
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// React Query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./App";
+// Custom contexts
 import ProjectsContextProvider from "./contexts/ProjectsContext";
+
+// Custom components
+import App from "./App";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
 });
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ProjectsContextProvider>
         <App />
       </ProjectsContextProvider>
     </QueryClientProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
