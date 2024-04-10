ee33ee75fecf0c0d2076c39285aed866ac65f445
theodoremoreland <theodoremoreland@yahoo.com>
2024-04-09 20:13:09 -0500

    removed bootstrap css import from index

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-// Bootstrap
-import "bootstrap/dist/css/bootstrap.min.css";
-
 // React Query
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
