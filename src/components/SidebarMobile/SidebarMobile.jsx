a14303d7e55a7635fadc0e32dec1ea378268cb62
theodoremoreland <theodoremoreland@yahoo.com>
2024-04-13 20:37:25 -0500

    added SidebarMobile component

---
+++ src/components/SidebarMobile/SidebarMobile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Components
+import TechList from '../TechList/TechList.jsx';
+
+// Styles
+import './SidebarMobile.css';
+
+const SidebarMobile = () => {
+    return (
+        <aside id="sidebar-mobile">
+            <TechList />
+        </aside>
+    )
+};
+
+
+export default SidebarMobile;
