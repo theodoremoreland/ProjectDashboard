05d53cd531d73248d90c8ab2d82e17fcbc63eddd
theodoremoreland <theodoremoreland@yahoo.com>
2024-04-19 12:54:14 -0500

    fixed style rule conflict for ids name content

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
           setShowAnalytics={setShowAnalytics}
           setShowHelpModal={setShowHelpModal}
         />
-        <div id="content">
+        <div id="app-content">
           <SidebarMobile />
           <Sidebar />
           {repos !== undefined ? (
