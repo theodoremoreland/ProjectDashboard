9518f1c048fdcedf05f4cef53675101a6cb2dd05
theodoremoreland <theodoremoreland@yahoo.com>
2024-06-02 21:46:07 -0500

    added comment with test results png vs webp load times

--- src/http/getRepoData.js
+++ src/http/getRepoData.js
@@ -27,6 +27,26 @@ export const getRepoData = async () => {
       date_created: covertDateToISOString(repo.created_at),
       date_updated: covertDateToISOString(repo.pushed_at),
       topics: repo.topics,
+      /**
+       * Changing from png to webp to reduce image size. This should help with load times and minimize
+       * data usage. Hopefully, it also helps with the frame rate of the animations on larger screens,
+       * namely ultra-wide monitors.
+       *
+       * So far, my tests have shown the following results (first test):
+       * Original images (png):
+       * 83 requests
+       * 21.12 MB / 20.26 MB transferred
+       * Finish: 1.73 s
+       * DOMContentLoaded: 280 ms
+       * load: 534 ms
+       *
+       * WebP images:
+       * 83 requests
+       * 6.71 MB / 3.14 MB transferred
+       * Finish: 1.40 s
+       * DOMContentLoaded: 26 ms
+       * load: 297 ms
+       */
       image: `https://raw.githubusercontent.com/theodoremoreland/${repo.name}/master/presentation/thumbnail.webp`,
       demo_link: repo.homepage,
       readme: `https://raw.githubusercontent.com/theodoremoreland/${repo.name}/master/README.md`,
