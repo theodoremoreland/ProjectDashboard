c48bf47f08f22d5dd50a002d5f9f79b42fa8dd98
theodoremoreland <theodoremoreland@yahoo.com>
2024-09-11 01:23:59 -0500

    updated util type def

--- vite-ts/src/utils/convertDateToISOString.ts
+++ vite-ts/src/utils/convertDateToISOString.ts
@@ -1,5 +1,5 @@
-const convertDateToISOString = (dateString: string): string => {
-  const dateObject: Date = new Date(dateString);
+const convertDateToISOString = (date: string | Date): string => {
+  const dateObject: Date = new Date(date);
   const UTCDateObject: Date = new Date(
     dateObject.getTime() - dateObject.getTimezoneOffset() * 60000
   );
