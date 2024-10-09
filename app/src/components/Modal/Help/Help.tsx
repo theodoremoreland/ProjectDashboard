bcfdbc235807ba28e0921ae87b12264b23341f6f
theodoremoreland <theodoremoreland@yahoo.com>
2024-10-09 17:59:19 -0500

    progress on update to info/help modal

--- app/src/components/Modal/Help/Help.tsx
+++ app/src/components/Modal/Help/Help.tsx
@@ -11,25 +11,33 @@ const Help = ({ handleClose }: Props): ReactElement => {
         <Modal title='Help' handleClose={handleClose}>
             <div>
                 <p> 
-                    This project dynamically renders a list of my GitHub repositories by leveraging
-                    the GitHub API. The projects featured meet a specific set of criteria and thus do
-                    not feature every repository that I own.
+                    This web application dynamically renders a list of my GitHub projects. Projects are
+                    queried in real-time from the GitHub API and are displayed in semi-random order. 
                 </p>
-                <ul>
-                    The following features are available
-                    <li>
-                        - Projects can be filtered by toggling items in the sidebar.
-                        Upon a project being filtered out, its thumbnail will lose opacity.
-                        Filters can be stacked.
-                    </li>
-                    <li>
-                        - Projects can be searched by name or GitHub topic in the search bar.
-                        Topics corresponding to the search query will be displayed in the search results.
-                    </li>
-                    <li>
-                        - Analytics can be viewed by clicking on the analytics icon in the navbar.
-                    </li>
-                </ul>
+                <article>
+                    <h3>Filter</h3>
+                    <p>
+                        Technologies listed on the left panel can be clicked to filter projects by corresponding technology.
+                        Projects not featuring said technology will be disabled from the list. The numbers displayed next to each
+                        technology represent the number of projects that utilize that technology.
+                    </p>
+                </article>
+                <article>
+                    <h3>Search</h3>
+                    <p>
+                        The search bar can be used to search for projects by name. The search is case-insensitive and will
+                        display projects that contain the search term in their name or in their list of GitHub topics.
+                    </p>
+                </article>
+                <article>
+                    <h3>Analytics</h3>
+                    <p>
+                        Clicking on the analytics icon in the navbar will open a small analytics dashboard 
+                    </p>
+                </article>
+                <p>
+                    The source code for this project can be viewed <a target='_blank' rel='noreferrer' href="https://github.com/theodoremoreland/ProjectList">here</a>.
+                </p>
             </div>
         </Modal>
     );
