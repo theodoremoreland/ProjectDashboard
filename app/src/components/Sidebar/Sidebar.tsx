a413ae354a57c1ada68fab609cdb58cd5827a779
theodoremoreland <theodoremoreland@yahoo.com>
2024-10-23 16:21:25 -0500

    added icons to techlist

--- app/src/components/Sidebar/Sidebar.tsx
+++ app/src/components/Sidebar/Sidebar.tsx
@@ -1,40 +1,36 @@
-import { ReactElement, useEffect, useState } from 'react';
+import { ReactElement, useEffect, useState } from "react";
 
 // Components
-import TechList from '../TechList/TechList';
+import TechList from "../TechList/TechList";
 
 // Images
 import DoubleArrowIcon from "../../images/icons/double-arrow-right.svg?react";
 
 // Styles
-import './Sidebar.css';
+import "./Sidebar.css";
 
 const Sidebar = (): ReactElement => {
-    const [isOpen, setIsOpen] = useState<boolean>(true);
+	const [isOpen, setIsOpen] = useState<boolean>(true);
 
-    useEffect(() => {
-        if (window.innerWidth < 769) {
-            setIsOpen(false);
-        }
-    }, []);
+	useEffect(() => {
+		if (window.innerWidth < 769) {
+			setIsOpen(false);
+		}
+	}, []);
 
-    return (
-        <aside
-            id="sidebar"
-            className={`${isOpen ? 'open' : 'closed'}`}
-        >
-            <button
-                type="button"
-                title="Toggle sidebar"
-                onClick={() => setIsOpen(!isOpen)}
-                className={`${isOpen ? 'open' : 'closed'}`}
-            >
-                <DoubleArrowIcon className='icon double-arrow' />
-            </button>
-            <TechList />
-        </aside>
-    )
+	return (
+		<aside id="sidebar" className={`${isOpen ? "open" : "closed"}`}>
+			<button
+				type="button"
+				title="Toggle sidebar"
+				onClick={() => setIsOpen(!isOpen)}
+				className={`${isOpen ? "open" : "closed"}`}
+			>
+				<DoubleArrowIcon className="icon double-arrow" />
+			</button>
+			<TechList />
+		</aside>
+	);
 };
-
 
 export default Sidebar;
--- app/src/components/TechList/TechList.tsx
+++ app/src/components/TechList/TechList.tsx
@@ -1,69 +1,101 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useCallback, useContext, useEffect, useState } from "react";
 
 // Controller
-import { findKeyForTopicLabel, getTopicCounts } from './TechList.controller';
+import {
+	findKeyForTopicLabel,
+	findTopicLabelImageSrc,
+	getTopicCounts,
+} from "./TechList.controller";
 
 // Context
-import { ProjectsContext } from '../../contexts/ProjectsContext'; 
+import { ProjectsContext } from "../../contexts/ProjectsContext";
 
 // Types
-import { TopicCounts } from '../../types';
+import { TopicCounts } from "../../types";
 
 // Styles
-import './TechList.css';
+import "./TechList.css";
 
 const TechList = () => {
-    const { repos, updateFeaturedTopics, featuredTopics } = useContext(ProjectsContext);
-    const [topicsCount, setTopicsCount] = useState<TopicCounts | null>(null);
+	const { repos, updateFeaturedTopics, featuredTopics } =
+		useContext(ProjectsContext);
+	const [topicsCount, setTopicsCount] = useState<TopicCounts | null>(null);
 
-    const generateListItems = useCallback((topics: { [key: string]: number }) => {
-        const determineClassName = (topicLabel: string) => {
-            if (featuredTopics.size === 0) {
-                return '';
-            }
+	const generateListItems = useCallback(
+		(topics: { [key: string]: number }) => {
+			const determineClassName = (topicLabel: string) => {
+				if (featuredTopics.size === 0) {
+					return "";
+				}
 
-            if (featuredTopics.has(findKeyForTopicLabel(topicLabel))) {
-                return 'selected';
-            }
+				if (featuredTopics.has(findKeyForTopicLabel(topicLabel))) {
+					return "selected";
+				}
 
-            return 'filtered-out';
-        }
+				return "filtered-out";
+			};
 
-        
-        return Object
-            .entries(topics)
-            .sort(([, topicCount1], [, topicCount2]) => topicCount2 - topicCount1)
-            .map(([topicLabel, topicCount]) => 
-                <li
-                    key={topicLabel}
-                    title={`Click to filter by ${topicLabel}`}
-                    className={`${determineClassName(topicLabel)}`}
-                    onClick={() => updateFeaturedTopics(findKeyForTopicLabel(topicLabel))}
-                >
-                    {topicLabel}:<span className='count'>{topicCount}</span>
-                </li>
-                );
-    }, [updateFeaturedTopics, featuredTopics]);
+			return Object.entries(topics)
+				.sort(
+					([, topicCount1], [, topicCount2]) =>
+						topicCount2 - topicCount1
+				)
+				.map(([topicLabel, topicCount]) => (
+					<li
+						key={topicLabel}
+						title={`Click to filter by ${topicLabel}`}
+						className={`${determineClassName(topicLabel)}`}
+						onClick={() =>
+							updateFeaturedTopics(
+								findKeyForTopicLabel(topicLabel)
+							)
+						}
+					>
+						{findTopicLabelImageSrc(topicLabel) ? (
+							<>
+								<div className="tech-icon-container">
+									<img
+										src={findTopicLabelImageSrc(topicLabel)}
+										alt={topicLabel}
+										className="tech-icon"
+									/>
+								</div>
+								<div className="topic-label-container">
+									{topicLabel}:
+									<span className="count">{topicCount}</span>
+								</div>
+							</>
+						) : (
+							<>
+								{" "}
+								{topicLabel}:
+								<span className="count">{topicCount}</span>
+							</>
+						)}
+					</li>
+				));
+		},
+		[updateFeaturedTopics, featuredTopics]
+	);
 
-    useEffect(() => {
-        if (repos) {
-            setTopicsCount(getTopicCounts(repos));
-        };
-    }, [repos]);
+	useEffect(() => {
+		if (repos) {
+			setTopicsCount(getTopicCounts(repos));
+		}
+	}, [repos]);
 
-    return (
-        <ul id="tech-list">
-            <p className='tech-category'>Competencies</p>
-            {topicsCount && generateListItems(topicsCount.competencies)}
-            <p className='tech-category'>Languages</p>
-            {topicsCount && generateListItems(topicsCount.languages)}
-            <p className='tech-category'>Frameworks</p>
-            {topicsCount && generateListItems(topicsCount.frameworks)}
-            <p className='tech-category'>Tools</p>
-            {topicsCount && generateListItems(topicsCount.tools)}
-        </ul>
-    )
+	return (
+		<ul id="tech-list">
+			<p className="tech-category">Competencies</p>
+			{topicsCount && generateListItems(topicsCount.competencies)}
+			<p className="tech-category">Languages</p>
+			{topicsCount && generateListItems(topicsCount.languages)}
+			<p className="tech-category">Frameworks</p>
+			{topicsCount && generateListItems(topicsCount.frameworks)}
+			<p className="tech-category">Tools</p>
+			{topicsCount && generateListItems(topicsCount.tools)}
+		</ul>
+	);
 };
-
 
 export default TechList;
