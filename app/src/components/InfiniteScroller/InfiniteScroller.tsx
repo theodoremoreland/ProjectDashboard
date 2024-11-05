4b095c24120ea7006f638b1a5c3f86e73b62e4a1
theodoremoreland <theodoremoreland@yahoo.com>
2024-11-04 21:52:20 -0600

    added InfiniteScroller boilerplate

---
+++ app/src/components/InfiniteScroller/InfiniteScroller.tsx
@@ -0,0 +1,11 @@
+// React
+import { ReactElement } from "react";
+
+// Styles
+import "./InfiniteScroller.css";
+
+const InfiniteScroller = (): ReactElement => {
+    return <div>Scroller</div>;
+};
+
+export default InfiniteScroller;
