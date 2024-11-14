27817ea5692758a4212869158d89555f2c983d63
theodoremoreland <theodoremoreland@yahoo.com>
2024-11-13 21:04:38 -0600

    progress on InfiniteScroller component

--- app/src/components/InfiniteScroller/InfiniteScroller.tsx
+++ app/src/components/InfiniteScroller/InfiniteScroller.tsx
@@ -4,8 +4,33 @@ import { ReactElement } from "react";
 // Styles
 import "./InfiniteScroller.css";
 
-const InfiniteScroller = (): ReactElement => {
-    return <div>Scroller</div>;
+interface Props {
+    items: (string | number)[];
+}
+
+const InfiniteScroller = ({ items }: Props): ReactElement => {
+    const halfwayPoint: number = Math.floor(items.length / 2);
+    const firstHalf: (string | number)[] = items.slice(0, halfwayPoint);
+    const secondHalf: (string | number)[] = items.slice(halfwayPoint);
+
+    return (
+        <div className="InfiniteScroller">
+            <div className="row">
+                {firstHalf.map((item, index) => (
+                    <span key={index} className="item">
+                        {item}
+                    </span>
+                ))}
+            </div>
+            <div className="row">
+                {secondHalf.map((item, index) => (
+                    <span key={index} className="item">
+                        {item}
+                    </span>
+                ))}
+            </div>
+        </div>
+    );
 };
 
 export default InfiniteScroller;
