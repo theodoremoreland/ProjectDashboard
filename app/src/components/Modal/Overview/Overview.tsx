ab2cf95e0ce99215436c84e9f497fd9b8054ce1e
Theodore Moreland <theodoremoreland@yahoo.com>
2025-07-30 17:02:24 -0500

    corrected and rephrased overview

--- app/src/components/Modal/Overview/Overview.tsx
+++ app/src/components/Modal/Overview/Overview.tsx
@@ -8,49 +8,45 @@ interface Props {
 
 const Overview = ({ handleClose }: Props): ReactElement => {
     return (
-        <Modal title="Overview" handleClose={handleClose}>
+        <Modal handleClose={handleClose}>
             <div>
-                <p>
-                    This web application dynamically renders a list of my{' '}
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://github.com/theodoremoreland?tab=repositories"
-                    >
-                        GitHub repositories
-                    </a>
-                    . Repositories are queried in real-time from the{' '}
-                    <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href="https://docs.github.com/en/rest"
-                    >
-                        GitHub API
-                    </a>
-                    .
-                </p>
                 <article>
                     <h3>Abstract</h3>
                     <p>
                         This project was originally designed around a pitch to
-                        my manager about moving laterally within the company
-                        from Data Engineer to Software Engineer. I intended to
-                        supplement an argument that my skills and interests were
-                        more aligned with software engineering through this
-                        dashboard. This project features search, filtering, and
-                        analytics related to my public GitHub repositories,
-                        providing the facts and figures needed to support my
-                        case.
+                        management about moving laterally within the company
+                        from Data Engineer to Software Engineer. To supplement
+                        my argument, this web application dynamically renders a
+                        list of my{' '}
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://github.com/theodoremoreland?tab=repositories"
+                        >
+                            GitHub repositories
+                        </a>
+                        . Repositories are queried in real-time from the{' '}
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://docs.github.com/en/rest"
+                        >
+                            GitHub API
+                        </a>
+                        . It features search, filtering, and analytics related
+                        to my public GitHub repositories, providing the facts
+                        and figures needed to support my case.
                     </p>
                     <p>
                         My argument ultimately succeeded after a series of
                         discussions, one of which involved a detailed
                         walkthrough of this project. I've since iterated on this
                         project and have occasionally positioned it as an
-                        interim portfolio. But naturally, given its original
-                        purpose, it lacks the resources and workflow of a
-                        typical portfolio project. There is no resume,
-                        head-shot, contact information, nor professional
+                        interim portfolio (i.e. a substitute for my "real"
+                        portfolio while I work on completing it). Naturally,
+                        given its original purpose, it lacks the resources and
+                        workflow of a typical portfolio project. There is no
+                        resume, head-shot, contact information, nor professional
                         summary, and it makes little effort to highlight
                         specific projects or skills.
                     </p>
@@ -62,9 +58,10 @@ const Overview = ({ handleClose }: Props): ReactElement => {
                         be visited by hovering over its corresponding thumbnail
                         and clicking the "Live Demo" button. Each active
                         deployment is responsive and deployed via CI/CD pipeline
-                        or scripted deployment. Everything else features a link
-                        to detailed READMEs on GitHub upon clicking the "Learn
-                        More" button.
+                        or scripted deployment. Every project features a GitHub
+                        link and screenshots pulled dynamically from their
+                        corresponding GitHub README.md, which can be explored
+                        via the "Explore" button.
                     </p>
                 </article>
                 <article>
