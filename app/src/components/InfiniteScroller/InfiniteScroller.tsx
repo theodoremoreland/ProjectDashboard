d951fe397266e92943b40bc8faa1af8914c22137
Theodore Moreland <theodoremoreland@yahoo.com>
2026-02-27 01:10:08 -0600

    infinite scroll of topics now requires touch/hover to trigger

--- app/src/components/InfiniteScroller/InfiniteScroller.tsx
+++ app/src/components/InfiniteScroller/InfiniteScroller.tsx
@@ -1,15 +1,17 @@
 // React
-import { ReactElement } from 'react';
+import { ReactElement, useRef } from 'react';
+
+// Images
+import HighlightMouseCursorIcon from '../../assets/images/icons/highlight_mouse_cursor.svg?react';
 
 // Styles
 import './InfiniteScroller.css';
 
 interface Props {
     items: (string | number)[];
-    scrollReady: boolean;
 }
 
-const InfiniteScroller = ({ items, scrollReady }: Props): ReactElement => {
+const InfiniteScroller = ({ items }: Props): ReactElement => {
     const halfwayPoint: number = Math.floor(items.length / 2);
     const firstHalf_Repeated: (string | number)[] = [
         ...items.slice(0, halfwayPoint),
@@ -20,16 +22,46 @@ const InfiniteScroller = ({ items, scrollReady }: Props): ReactElement => {
         ...items.slice(halfwayPoint),
     ];
 
+    const leftLane = useRef<HTMLUListElement>(null);
+    const rightLane = useRef<HTMLUListElement>(null);
+
+    const activateLanes = (): void => {
+        if (leftLane.current) {
+            leftLane.current.classList.add('active');
+        }
+        if (rightLane.current) {
+            rightLane.current.classList.add('active');
+        }
+    };
+
+    const deactivateLanes = (): void => {
+        if (leftLane.current) {
+            leftLane.current.classList.remove('active');
+        }
+        if (rightLane.current) {
+            rightLane.current.classList.remove('active');
+        }
+    };
+
     return (
         <div className="InfiniteScroller">
-            <ul className={`lane ${scrollReady ? 'left' : ''}`}>
+            <div
+                className="hover-indicator-container"
+                onMouseEnter={activateLanes}
+                onMouseLeave={deactivateLanes}
+                onTouchStart={activateLanes}
+                onTouchEnd={deactivateLanes}
+            >
+                <HighlightMouseCursorIcon className="hover-indicator" />
+            </div>
+            <ul ref={leftLane} className="lane left">
                 {firstHalf_Repeated.map((item, index) => (
                     <li key={`${item}-${index}`} className="item topic">
                         {item}
                     </li>
                 ))}
             </ul>
-            <ul className={`lane ${scrollReady ? 'right' : ''}`}>
+            <ul ref={rightLane} className="lane right">
                 {secondHalf_Repeated.map((item, index) => (
                     <li key={`${item}-${index}`} className="item topic">
                         {item}
