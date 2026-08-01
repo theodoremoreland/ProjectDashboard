d81d7f0c0f7336fd947137903a1bf01dc0e66d15
Theodore Moreland <theodoremoreland@yahoo.com>
2026-08-01 00:24:16 -0500

    progress

--- app/src/components/Sidebar/Sidebar.tsx
+++ app/src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,19 @@ import { ReactElement } from 'react';
 import './Sidebar.css';
 
 const Sidebar = (): ReactElement => {
-    return <aside id="sidebar"></aside>;
+    return (
+        <aside id="sidebar">
+            <ul id="sidebar-list">
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+                    <li key={item}>
+                        <h3>Technology {item}</h3>
+                        <div></div>
+                    </li>
+                ))}
+            </ul>
+            <div id="sidebar-video"></div>
+        </aside>
+    );
 };
 
 export default Sidebar;
