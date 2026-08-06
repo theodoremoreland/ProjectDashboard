d68b7558da9bfa4e574abb5616a10513472aeff6
Theodore Moreland <theodoremoreland@yahoo.com>
2026-08-06 03:03:36 -0500

    added trailing text to project cards

--- app/src/components/ProjectRow/Cards/MetricsCard.tsx
+++ app/src/components/ProjectRow/Cards/MetricsCard.tsx
@@ -10,16 +10,35 @@ const MetricsCard = ({
     projectData: TaggedRepoData;
 }): ReactElement => {
     return (
-        <li className="project-card metrics">
-            <h3>Metrics</h3>
-            <ul>
-                <li>Deployment Frequency</li>
-                <li>Lead Time for Changes</li>
-                <li>Change Failure Rate</li>
-                <li>Failed Deployment Recovery Time</li>
-                <li>Time to Restore Service</li>
-            </ul>
-            <p>{projectData.name}</p>
+        <li className="project-card-container">
+            <div className="project-card MetricsCard">
+                <h3>Metrics</h3>
+                <ul>
+                    <li>Deployment Frequency</li>
+                    <li>Lead Time for Changes</li>
+                    <li>Change Failure Rate</li>
+                    <li>Failed Deployment Recovery Time</li>
+                    <li>Time to Restore Service</li>
+                </ul>
+                {projectData.demo_link &&
+                    projectData.name !== 'ProjectDashboard' && (
+                        <a
+                            className="live-demo-link interactive"
+                            href={projectData.demo_link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title={`Click to view a live deployment of the ${projectData.name} project.`}
+                        >
+                            <button className="live-demo">
+                                View Deployment <span className="circle"></span>
+                            </button>
+                        </a>
+                    )}
+            </div>
+            <div className="trailing-text">
+                <p>Metrics</p>
+                <p>{projectData.id}</p>
+            </div>
         </li>
     );
 };
