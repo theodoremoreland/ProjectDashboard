33ce798392b9ae9d6d9a85e1690fe5f02fc3a23c
Theodore Moreland <theodoremoreland@yahoo.com>
2026-08-11 16:31:10 -0500

    styling and adding date to header

--- app/src/components/ProjectRow/Sidebar/Sidebar.tsx
+++ app/src/components/ProjectRow/Sidebar/Sidebar.tsx
@@ -7,11 +7,12 @@ import './Sidebar.css';
 const Sidebar = (): ReactElement => {
     return (
         <aside className="sidebar">
+            <h3 className="sidebar-title">Languages //</h3>
             <ul className="sidebar-list">
                 {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
                     <li key={item}>
-                        <h3>Technology {item}</h3>
-                        <span className="technology-level"></span>
+                        <h3>Language {item}</h3>
+                        <span className="language-level"></span>
                     </li>
                 ))}
             </ul>
