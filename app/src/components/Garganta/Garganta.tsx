c27b8b8a64d31340e522146ba46b8a3398586c0e
Theodore Moreland <theodoremoreland@yahoo.com>
2026-08-28 06:46:07 -0500

    added placeholder for Garganta component

---
+++ app/src/components/Garganta/Garganta.tsx
@@ -0,0 +1,14 @@
+// React
+import { ReactElement } from 'react';
+
+// Styles
+import './Garganta.css';
+
+const Garganta = (): ReactElement => {
+    return (
+        <div className="Garganta">
+        </div>
+    );
+}
+
+export default Garganta;
