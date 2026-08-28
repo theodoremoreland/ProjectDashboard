3953a25fcbccb0ddc93dd4a8dac83da49b14ad95
Theodore Moreland <theodoremoreland@yahoo.com>
2026-08-28 06:59:59 -0500

    progress on styles

--- app/src/components/Garganta/Garganta.tsx
+++ app/src/components/Garganta/Garganta.tsx
@@ -7,8 +7,11 @@ import './Garganta.css';
 const Garganta = (): ReactElement => {
     return (
         <div className="Garganta">
+            {Array.from({ length: 18 }).map((_, index) => (
+                <div key={index} className="Garganta__line" />
+            ))}
         </div>
     );
-}
+};
 
 export default Garganta;
