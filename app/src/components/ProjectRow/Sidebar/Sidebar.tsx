5f0684879b7f1db61e8a51a806a969a53a854a74
Theodore Moreland <theodoremoreland@yahoo.com>
2026-08-30 04:25:58 -0500

    first draft of language percentages

--- app/src/components/ProjectRow/Sidebar/Sidebar.tsx
+++ app/src/components/ProjectRow/Sidebar/Sidebar.tsx
@@ -1,6 +1,9 @@
 // React
 import { ReactElement } from 'react';
 
+// Custom
+import { getPercentages } from '../../../utils/getPercentages';
+
 // Styles
 import './Sidebar.css';
 
@@ -13,21 +16,32 @@ const Sidebar = ({
     languages,
     isTopLanguagesFetching,
 }: Props): ReactElement => {
+    const percentages = getPercentages(languages || {});
+
     return (
         <aside className="sidebar">
             <h3 className="sidebar-title">Languages //</h3>
             <ul className="sidebar-list">
                 {isTopLanguagesFetching && <p>Loading top languages...</p>}
-                {languages &&
-                    Object.entries(languages).map(([language, percentage]) => (
-                        <li key={language}>
-                            <span className="language-label">
-                                <h3>{language}</h3>
-                                <p>{percentage}%</p>
-                            </span>
-                            <span className="language-level"></span>
-                        </li>
-                    ))}
+                {percentages &&
+                    Object.entries(percentages).map(
+                        ([language, percentage]) => (
+                            <li key={language}>
+                                <span className="language-label">
+                                    <h3>{language}</h3>
+                                    <p>{percentage.toFixed(2)}%</p>
+                                </span>
+                                <div className="language-level-container">
+                                    <span
+                                        className="language-level"
+                                        style={{
+                                            width: `${percentage.toFixed(2)}%`,
+                                        }}
+                                    ></span>
+                                </div>
+                            </li>
+                        )
+                    )}
             </ul>
         </aside>
     );
