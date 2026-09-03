81cfcbd68b53b3b1a3d7b6de95869013a0346372
Theodore Moreland <theodoremoreland@yahoo.com>
2026-09-03 10:38:33 -0500

    added placeholder for GradeGraph component and refactored metrics card

--- app/src/components/ProjectRow/Cards/MetricsCard.tsx
+++ app/src/components/ProjectRow/Cards/MetricsCard.tsx
@@ -7,6 +7,7 @@ import { REPO_OWNER } from '../../../constants/RepoOwner';
 
 // Components
 import Corner from '../../Corner/Corner';
+import GradeGraph from '../../GradeGraph/GradeGraph';
 
 // Types
 import { SonarMeasures, TaggedRepoData } from '../../../types';
@@ -38,191 +39,173 @@ const MetricsCard = ({
             <div className="project-card MetricsCard">
                 <Corner position="top-left" />
                 <Corner position="bottom-right" />
-                <div className="dora-container">
-                    <ul className="metrics-list">
-                        <li>
-                            <p>Deployment Frequency</p>
-                            <p className="grade">2pw</p>
-                        </li>
-                        <li>
-                            <p>Lead Time for Changes</p>
-                            <p className="grade">1w</p>
-                        </li>
-                        <li>
-                            <p>Change Failure Rate</p>
-                            <p className="grade">5%</p>
-                        </li>
-                        <li>
-                            <p>Failed Deployment Recovery Time</p>
-                            <p className="grade">2h</p>
-                        </li>
-                        <li>
-                            <p>Time to Restore Service</p>
-                            <p className="grade">30m</p>
-                        </li>
-                    </ul>
-                </div>
-                <div className="metrics-label-container">
-                    <h3>DORA Metrics</h3>x<h3>Software Quality</h3>
-                </div>
-                <div className="software-quality-container">
-                    <ul className="metrics-list">
-                        <li>
-                            <div>
-                                <p>Maintainability</p>
-                                <div className="level-container">
-                                    <span
-                                        className="level"
-                                        style={{
-                                            width: `100%`,
-                                        }}
-                                    ></span>
-                                </div>
-                            </div>
-                            {isSonarMeasuresFetching ? (
-                                <p className="grade">Fetching...</p>
-                            ) : (
-                                <a
-                                    className="grade-link interactive"
-                                    href={SoftwareQualityLink.maintainability}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    title={`Click to view the maintainability rating for the ${projectData.name} project on SonarCloud.`}
-                                >
-                                    <p
-                                        className={`grade ${convertToSonarGrade(
-                                            sonarMeasures?.metrics.sqale_rating
-                                        )}`}
-                                    >
-                                        {convertToSonarGrade(
-                                            sonarMeasures?.metrics.sqale_rating
-                                        )}
-                                    </p>
-                                </a>
-                            )}
-                        </li>
-                        <li>
-                            <div>
-                                <p>Reliability</p>
-                                <div className="level-container">
-                                    <span
-                                        className="level"
-                                        style={{
-                                            width: `100%`,
-                                        }}
-                                    ></span>
-                                </div>
-                            </div>
-                            {isSonarMeasuresFetching ? (
-                                <p className="grade">Fetching...</p>
-                            ) : (
-                                <a
-                                    className="grade-link interactive"
-                                    href={SoftwareQualityLink.reliability}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    title={`Click to view the reliability rating for the ${projectData.name} project on SonarCloud.`}
-                                >
-                                    <p
-                                        className={`grade ${convertToSonarGrade(
-                                            sonarMeasures?.metrics
-                                                .reliability_rating
-                                        )}`}
-                                    >
-                                        {convertToSonarGrade(
-                                            sonarMeasures?.metrics
-                                                .reliability_rating
-                                        )}
-                                    </p>
-                                </a>
-                            )}
-                        </li>
-                        <li>
-                            <div>
-                                <p>Security</p>
-                                <div className="level-container">
-                                    <span
-                                        className="level"
-                                        style={{
-                                            width: `100%`,
-                                        }}
-                                    ></span>
-                                </div>
-                            </div>
-                            {isSonarMeasuresFetching ? (
-                                <p className="grade">Fetching...</p>
-                            ) : (
-                                <a
-                                    className="grade-link interactive"
-                                    href={SoftwareQualityLink.security}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    title={`Click to view the security rating for the ${projectData.name} project on SonarCloud.`}
-                                >
-                                    <p
-                                        className={`grade ${convertToSonarGrade(sonarMeasures?.metrics.security_rating)}`}
-                                    >
-                                        {convertToSonarGrade(
-                                            sonarMeasures?.metrics
-                                                .security_rating
-                                        )}
-                                    </p>
-                                </a>
-                            )}
-                        </li>
-                        <li>
-                            <div>
-                                <p>Test Coverage</p>
-                                <div className="level-container">
-                                    <span
-                                        className="level"
-                                        style={{
-                                            width: `100%`,
-                                        }}
-                                    ></span>
-                                </div>
-                            </div>
-                            {isSonarMeasuresFetching ? (
-                                <p className="grade">Fetching...</p>
-                            ) : (
+                <ul className="software-quality-container">
+                    <li>
+                        <div>
+                            <p>Lines of Code</p>
+                            <GradeGraph grade="N/A" />
+                        </div>
+                        {isSonarMeasuresFetching ? (
+                            <p className="grade">...</p>
+                        ) : (
+                            <p className="grade">
+                                {sonarMeasures?.metrics.ncloc}
+                            </p>
+                        )}
+                    </li>
+                    <li>
+                        <div>
+                            <p>Maintainability</p>
+                            <GradeGraph grade="N/A" />
+                        </div>
+                        {isSonarMeasuresFetching ? (
+                            <p className="grade">...</p>
+                        ) : (
+                            <a
+                                className="grade-link interactive"
+                                href={SoftwareQualityLink.maintainability}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                title={`Click to view the maintainability rating on SonarCloud.`}
+                            >
                                 <p
                                     className={`grade ${convertToSonarGrade(
-                                        sonarMeasures?.metrics.coverage
+                                        sonarMeasures?.metrics.sqale_rating
                                     )}`}
                                 >
                                     {convertToSonarGrade(
-                                        sonarMeasures?.metrics.coverage
+                                        sonarMeasures?.metrics.sqale_rating
                                     )}
                                 </p>
-                            )}
-                        </li>
-                    </ul>
+                            </a>
+                        )}
+                    </li>
+                    <li>
+                        <div>
+                            <p>Reliability</p>
+                            <GradeGraph grade="N/A" />
+                        </div>
+                        {isSonarMeasuresFetching ? (
+                            <p className="grade">...</p>
+                        ) : (
+                            <a
+                                className="grade-link interactive"
+                                href={SoftwareQualityLink.reliability}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                title={`Click to view the reliability rating on SonarCloud.`}
+                            >
+                                <p
+                                    className={`grade ${convertToSonarGrade(
+                                        sonarMeasures?.metrics
+                                            .reliability_rating
+                                    )}`}
+                                >
+                                    {convertToSonarGrade(
+                                        sonarMeasures?.metrics
+                                            .reliability_rating
+                                    )}
+                                </p>
+                            </a>
+                        )}
+                    </li>
+                    <li>
+                        <div>
+                            <p>Security</p>
+                            <GradeGraph grade="N/A" />
+                        </div>
+                        {isSonarMeasuresFetching ? (
+                            <p className="grade">...</p>
+                        ) : (
+                            <a
+                                className="grade-link interactive"
+                                href={SoftwareQualityLink.security}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                title={`Click to view the security rating on SonarCloud.`}
+                            >
+                                <p
+                                    className={`grade ${convertToSonarGrade(sonarMeasures?.metrics.security_rating)}`}
+                                >
+                                    {convertToSonarGrade(
+                                        sonarMeasures?.metrics.security_rating
+                                    )}
+                                </p>
+                            </a>
+                        )}
+                    </li>
+                    <li>
+                        <div>
+                            <p>Test Coverage</p>
+                            <GradeGraph grade="N/A" />
+                        </div>
+                        {isSonarMeasuresFetching ? (
+                            <p className="grade">...</p>
+                        ) : (
+                            <p
+                                className={`grade ${convertToSonarGrade(
+                                    sonarMeasures?.metrics.coverage
+                                )}`}
+                            >
+                                {convertToSonarGrade(
+                                    sonarMeasures?.metrics.coverage
+                                )}
+                            </p>
+                        )}
+                    </li>
+                </ul>
+                <div className="metrics-label-container">
+                    <h3>Software Quality</h3>x<h3>DORA Metrics</h3>
                 </div>
-                <div className="deployment-button-container">
-                    <a
-                        className={`view-deployment-link ${hasValidDemoLink ? 'interactive' : ''}`}
-                        href={
-                            hasValidDemoLink ? projectData.demo_link : undefined
-                        }
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        title={
-                            hasValidDemoLink
-                                ? `Click to view a live deployment of the ${projectData.name} project.`
-                                : undefined
-                        }
-                    >
-                        <button
-                            className={`view-deployment ${hasValidDemoLink ? 'interactive' : 'disabled'}`}
-                            disabled={!hasValidDemoLink}
+                <ul className="dora-container">
+                    <li>
+                        <p>Deployment Frequency</p>
+                        <p className="grade">2pw</p>
+                    </li>
+                    <li>
+                        <p>Lead Time for Changes</p>
+                        <p className="grade">1w</p>
+                    </li>
+                    <li>
+                        <p>Change Failure Rate</p>
+                        <p className="grade">5%</p>
+                    </li>
+                    <li>
+                        <p>Failed Deployment Recovery Time</p>
+                        <p className="grade">2h</p>
+                    </li>
+                    <li>
+                        <p>Time to Restore Service</p>
+                        <p className="grade">30m</p>
+                    </li>
+                    <div className="deployment-button-container">
+                        <a
+                            className={`view-deployment-link ${hasValidDemoLink ? 'interactive' : ''}`}
+                            href={
+                                hasValidDemoLink
+                                    ? projectData.demo_link
+                                    : undefined
+                            }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title={
+                                hasValidDemoLink
+                                    ? `Click to view a live deployment of the ${projectData.name} project.`
+                                    : undefined
+                            }
                         >
-                            <span>View Deployment</span>{' '}
-                            {hasValidDemoLink && (
-                                <span className="circle"></span>
-                            )}
-                        </button>
-                    </a>
-                </div>
+                            <button
+                                className={`view-deployment ${hasValidDemoLink ? 'interactive' : 'disabled'}`}
+                                disabled={!hasValidDemoLink}
+                            >
+                                <span>View Deployment</span>{' '}
+                                {hasValidDemoLink && (
+                                    <span className="circle"></span>
+                                )}
+                            </button>
+                        </a>
+                    </div>
+                </ul>
             </div>
             <div className="trailing-text">
                 <p>Metrics</p>
