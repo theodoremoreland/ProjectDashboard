d73069f9c26b721ee3dc99912f272cbecd9a2484
Theodore Moreland <theodoremoreland@yahoo.com>
2026-09-08 04:27:29 -0500

    starting work on Glyph component

---
+++ app/src/components/GlyphLane/GlyphLane.tsx
@@ -0,0 +1,32 @@
+// React
+import { ReactElement, useMemo } from 'react';
+
+// Styles
+import 'GlyphLane.css';
+
+const glyphs: string[] = ['#', '/', '-', '*', '='];
+
+const GlyphLane = (): ReactElement => {
+    const randomAssortment = useMemo(() => {
+        const result: string[] = [];
+
+        while (result.length < 50) {
+            const randomGlyph: string =
+                glyphs[Math.round(Math.random() * (glyphs.length - 1))];
+
+            result.push(randomGlyph);
+        }
+
+        return result;
+    }, []);
+
+    return (
+        <div className="GlyphLane">
+            {randomAssortment.map((glyph) => (
+                <span className="glyph">{glyph}</span>
+            ))}
+        </div>
+    );
+};
+
+export default GlyphLane;
