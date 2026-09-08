4986527f40ee9a892f3c2257343557984e6885f0
Theodore Moreland <theodoremoreland@yahoo.com>
2026-09-08 10:38:48 -0500

    fixed styling for GlyphLane

--- app/src/components/GlyphLane/GlyphLane.tsx
+++ app/src/components/GlyphLane/GlyphLane.tsx
@@ -1,25 +1,21 @@
 // React
-import { ReactElement, useMemo } from 'react';
+import { ReactElement } from 'react';
 
 // Styles
 import './GlyphLane.css';
 
 const glyphs: string[] = ['#', '/', '-', '*', '='];
 
+const randomAssortment: string[] = [];
+
+for (let i = 0; i < 49; i++) {
+    const randomGlyph: string =
+        glyphs[Math.round(Math.random() * (glyphs.length - 1))];
+
+    randomAssortment.push(randomGlyph);
+}
+
 const GlyphLane = (): ReactElement => {
-    const randomAssortment: string[] = useMemo(() => {
-        const result: string[] = [];
-
-        for (let i = 0; i < 49; i++) {
-            const randomGlyph: string =
-                glyphs[Math.round(Math.random() * (glyphs.length - 1))];
-
-            result.push(randomGlyph);
-        }
-
-        return result;
-    }, []);
-
     return (
         <div className="GlyphLane">
             {randomAssortment.map((glyph, index) => (
