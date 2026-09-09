5cee71daee152cd7e468bde5a5fc1014e8262fae
Theodore Moreland <theodoremoreland@yahoo.com>
2026-09-09 05:44:43 -0500

    project names + barcodes now appear on hover for NotAScrollbar list items

--- app/src/components/Analytics/Analytics.tsx
+++ app/src/components/Analytics/Analytics.tsx
@@ -222,7 +222,7 @@ const Analytics = ({ projects, handleClose }: Props): ReactElement => {
                     <button
                         type="button"
                         title="Close"
-                        className="close-button interactive"
+                        className="close-button"
                     >
                         Close
                     </button>
